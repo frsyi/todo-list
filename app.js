02e8b273ccbccd1ee7d57b913e2aa524666b08ff
frsyi <fahmarosyi@gmail.com>
2025-10-15 21:28:18 +0700

    feat: add routes for auth

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./src/config/db");
+const allRoutes = require("./src/routes");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
@@ -10,6 +11,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to ToDoList API!" });
 });
+
+app.use(allRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
