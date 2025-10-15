a2b0e1f075c91e866b144c50c15ccb30ec937fb0
frsyi <fahmarosyi@gmail.com>
2025-10-15 18:10:20 +0700

    feat: connect to MongoDB using Mongoose

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./src/config/db");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
+connectDB();
 
 app.use(express.json());
-
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to ToDoList API!" });
 });
