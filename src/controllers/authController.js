dccfb72dc5ea05b22f195a80f420cbb807f5ae2c
frsyi <fahmarosyi@gmail.com>
2025-10-15 21:14:57 +0700

    feat: user login controller

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -25,4 +25,26 @@ module.exports = {
         .json({ message: "Error registering user", error: error.message });
     }
   },
+
+  login: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      if (!email || !password) {
+        return res
+          .status(400)
+          .json({ message: "Email and password are required" });
+      }
+
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+
+      res.status(200).json({ message: "User logged in successfully" });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "Error logging in user", error: error.message });
+    }
+  },
 };
