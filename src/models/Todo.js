8e09d7dd9f8548817fa246cc36edb90f9668ca83
frsyi <fahmarosyi@gmail.com>
2025-10-16 16:17:16 +0700

    feat: create Todo model

---
+++ src/models/Todo.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const todoSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      default: "",
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Todo = mongoose.model("Todo", todoSchema);
+module.exports = Todo;
